41b73aa568cc7487c18856bf80fc9ccc37ee3b4a
Nathan Clarke <CaptainCow95@gmail.com>
2014-09-25 13:38:30 -0600

    ReSharper changes
    Ran ReSharper and made some changes.

--- Program.cs
+++ Program.cs
@@ -1,30 +1,31 @@
-﻿using System;
+﻿using MeshNetwork;
+using System;
 using System.Linq;
 using System.Threading;
 
 namespace MeshNetworkTester
 {
-    internal class Program
+    public static class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             Console.Write("Input a port to run on: ");
             int port = int.Parse(Console.ReadLine());
             Console.Write("Input a comma separated list of servers to connect to: ");
             string servers = Console.ReadLine();
-            MeshNetwork.NetworkNode node = new MeshNetwork.NetworkNode("MeshNetworkTester" + port + ".log");
-            node.ConnectToNetwork(port, servers.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(e => new MeshNetwork.NodeProperties(e)));
+            NetworkNode node = new NetworkNode("MeshNetworkTester" + port + ".log");
+            node.ConnectToNetwork(port, servers.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(e => new NodeProperties(e)));
 
-            MeshNetwork.NodeProperties thisMachine = new MeshNetwork.NodeProperties("127.0.0.1", port);
+            var thisMachine = new NodeProperties("127.0.0.1", port);
             while (true)
             {
                 Console.Clear();
-                Console.WriteLine("Currently running on " + thisMachine.IP.ToString() + ":" + thisMachine.Port.ToString());
+                Console.WriteLine("Currently running on " + thisMachine.IpAddress + ":" + thisMachine.Port);
                 Console.WriteLine();
                 Console.WriteLine("Connected Nodes:");
                 foreach (var neighbor in node.GetNeighbors())
                 {
-                    Console.WriteLine(neighbor.IP + ":" + neighbor.Port);
+                    Console.WriteLine(neighbor.IpAddress + ":" + neighbor.Port);
                 }
 
                 Thread.Sleep(5000);
