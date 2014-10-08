932f840e18d999dc7a58ee7b5d9aaec13b1646db
Nathan Clarke <CaptainCow95@gmail.com>
2014-10-07 23:30:49 -0600

    Updating library
    Some code fixes so that it works with the newest library commit.

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace MeshNetworkTester
             Console.Write("Input a comma separated list of servers to connect to: ");
             string servers = Console.ReadLine();
             NetworkNode node = new NetworkNode("MeshNetworkTester" + port + ".log");
-            node.ConnectToNetwork(port, servers.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(e => new NodeProperties(e)));
+            node.ConnectToNetworkAsync(port, servers.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(e => new NodeProperties(e)));
 
             var thisMachine = new NodeProperties("localhost", port);
             while (true)
